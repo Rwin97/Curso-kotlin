8daccdf05993200bac6976c0ef4b777aa7670f30
Rwin97 <leonerwin890@gmail.com>
2021-03-28 22:27:21 -0400

    practica de string

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,20 +1,20 @@
 fun main(args: Array<String>) {
     println("Hola mundo desde platzi")
 
-    println(1+1)
-    println(3-1)
-    println(2*2)
-    println(4/2)
-    println(7%2)
-    println("\n")
-
-    val a = 4
-    val b = 2
-
-    println(a.plus(b))
-    println(a.minus(b))
-    println(a.times(b))
-    println(a.div(b))
+   val nombre = "Erwin"
+   val apellido: String = "Leon"
+   val a: Int = 4 //buena practica definir el tipo de dato
+   println("Tu nombre es: $nombre $apellido")
+    //raw String
+    val nombreApellido = "Erwin \"Leon\""
+    println("Tu nombre es: $nombreApellido")
+    val parrafo ="""
+        ** Lorem Ipsum es un texto que parece latín, 
+        ** pero en realidad no tiene sentido tampoco 
+        ** en este idioma. Su título deriva del latín 
+        ** dolorem ipsum, que significa «el dolor en sí».
+    """.trimIndent()
+    println(parrafo.trimMargin("** "))
 
 
 
