c2993c481f36bad553c677dcd4438881bf6ecd44
Rwin97 <leonerwin890@gmail.com>
2021-03-28 23:25:00 -0400

    rangos, sentecnias y bucles

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,21 +1,53 @@
 fun main(args: Array<String>) {
     println("Hola mundo desde platzi")
 
-    println(1+1)
-    println(3-1)
-    println(2*2)
-    println(4/2)
-    println(7%2)
-    println("\n")
+    var a = 5
+    var b: Long = a.toLong()
+    println(b)
 
-    val a = 4
-    val b = 2
+    val nombre = args[0].toInt()
+    val c = a.plus(nombre)
+    println(c)
 
-    println(a.plus(b))
-    println(a.minus(b))
-    println(a.times(b))
-    println(a.div(b))
+    //Rangos
 
+    val oneToHumdred = 1..5
+    for (i in oneToHumdred){
+        println(i)
+    }
 
+    //val aToC = 'a'..'c'
+    for (i in 'a'..'c'){
+        println(i)
+    }
 
+    //sentecia if es un operador logico booleano es decir devuelve true o false
+    val num1 = 10
+    if (num1.equals(2)){
+        println("si son iguales")
+    }else{
+        println("no, no son iguales")
+    }
+
+    // when es equivalente a switch
+    when(num1){
+        in 1..5 -> println("si esta entre 1 y 5")
+        in 1..3 -> println("si esta entre 1 y tres")
+        !in 5..10 -> println("No, no esta entre 5 y 10")
+        else -> println("no esta en ninguno de los anteriores")
+
+    }
+
+    //bucles
+    var cont = 1
+    //
+    while (cont < 1){
+        println("mensaje: $cont")
+        cont++
+    }
+
+    do {
+        println("mensaje do while: $cont")
+        cont++
+    }while (cont < 1)
 }
