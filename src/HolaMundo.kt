a3d2c7cc36cde92bb9a86822dcb5bafaee348598
Rwin97 <leonerwin890@gmail.com>
2021-03-30 11:09:09 -0400

    ejercicios de arrays

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,21 +1,33 @@
 fun main(args: Array<String>) {
     println("Hola mundo desde platzi")
-
-    println(1+1)
-    println(3-1)
-    println(2*2)
-    println(4/2)
-    println(7%2)
-    println("\n")
-
-    val a = 4
-    val b = 2
-
-    println(a.plus(b))
-    println(a.minus(b))
-    println(a.times(b))
-    println(a.div(b))
+    //Array
+    val countries = arrayOf("India", "Mexico", "Colombia", "Argentia", "Chile", "Peru")
+    val days = arrayOf<String>("Lunes", "Martes", "Miercoles")
 
 
+    //promedio de los numeros del arreglo
+    val numbers = intArrayOf(1, 2, 3, 4, 5)
+    var sum = 0
+    for (num in numbers){
+        println("Numbers: $num")
+        sum += num
 
+    }
+    val avarage = sum / numbers.size
+    println("Promedio: $avarage")
+
+    var arrayObject: Array<Int> = arrayOf(5,6,7)
+    var inPrimitive: IntArray = arrayObject.toIntArray()
+    arrayObject = arrayObject.plus(2) //agregar un elemento a nuestro array
+
+    val suma = arrayObject.sum()
+    println("La suma de la array es: $suma")
+    for(a in arrayObject){
+        println("Array: $a")
+    }
+
+    arrayObject = arrayObject.reversedArray() //devuelve el arreglo en reversa
+    for (a in arrayObject){
+        println("Array reverso: $a")
+    }
 }
