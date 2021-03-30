323aa247d46bc15b000fe9e126fb44127215346d
Rwin97 <leonerwin890@gmail.com>
2021-03-29 21:46:38 -0400

    ejercicios de manejo de exepcione

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,21 +1,34 @@
+
+
 fun main(args: Array<String>) {
     println("Hola mundo desde platzi")
 
-    println(1+1)
-    println(3-1)
-    println(2*2)
-    println(4/2)
-    println(7%2)
-    println("\n")
-
-    val a = 4
-    val b = 2
-
-    println(a.plus(b))
-    println(a.minus(b))
-    println(a.times(b))
-    println(a.div(b))
+    //Ejemplo null safety
+    //con el signo de interrogacion recive el valor nulo
+    //si usamos double bang simpre tiene que ir con su cathc
+    /*try {
+    var compute: String? = null
+    var longitud: Int = compute!!.length
 
 
+
+    }catch (e: NullPointerException){
+        println("no se aceptan valores nulos")
+    }*/
+    //llamada segura evita caer en execpciones
+    var compute: String? = null
+    var longitud: Int? = compute?.length // null
+    println("Longitude: $longitud")
+
+    //operador elvis
+
+    var teclado: String? = null
+    var longitudeTeclado: Int = teclado?.length ?: 0
+    println("Longitud del teclado: $longitudeTeclado")
+
+    var listWhitNulls: List<Int?> = listOf<Int?>(7, null, null)
+    println("Lista con null: ${listWhitNulls}")
+    val listWithoutNulls: List<Int?> = listWhitNulls.filterNotNull() // filtra lo que nos siver es decir solo queremos que nos imprima los valores no nulos
+    println(listWithoutNulls)
 
 }
