d77aba407c1d4b2adcf277c4186db23ee1500876
Rwin97 <leonerwin890@gmail.com>
2021-04-02 00:48:30 -0400

    final de curso

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -41,4 +41,18 @@ fun main(args: Array<String>) {
     println(movie.title)
     println(movie.duration)
     println("${movie.duration} min.")
+
+var resultado: Int = calculadora(1, 2, 3,::multiplicar)
+println("El resultado es: $resultado ")
+
+    println("El resultado es: ${calculadora(4, 5, 6, ::restar)} ")
+
 }
+
+fun calculadora(a: Int, b: Int, c: Int, operacion:(Int, Int, Int)-> Int): Int{
+    return operacion(a, b, c)
+}
+
+fun sumar(a: Int, b: Int, c: Int) = a+b+c
+fun restar(a: Int, b: Int, c: Int) = a-b-c
+fun multiplicar(a: Int, b: Int, c: Int) = a*b*c
